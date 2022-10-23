f568c75ff849afd002b27de40019a79bd68aef0f
Owen Lovett <ollovett@cpp.edu>
2022-10-22 23:03:21 -0700

    init outline

---
+++ bill.c
@@ -0,0 +1,11 @@
+// struct with food names and prices
+
+// const with food names and prices
+
+// main
+    // pass in arguments for tax and tip
+    // generate random number and use that number to select random food item
+    // calculate tax
+    // calculate tip
+    // total bill
+    // print food item and cost, tax, tip, and total bill
